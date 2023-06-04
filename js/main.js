04c04884ffa08d0dfb93600c1a842f05ec656251
dilushaeka <dilushaeka99@gmail.com>
2023-06-04 10:32:56 +0530

    Responsive all sections

--- js/main.js
+++ js/main.js
@@ -1,145 +1,17 @@
+
+
 window.addEventListener("scroll", function () {
   
   var header = this.document.querySelector("header");
   header.classList.toggle("sticky",window.scrollY > 0);
 });
 
+// const button = document.querySelector(".mobile-nav-toggle");
+
+// i.addEventListener("click", (event) => {
+//   button.textContent = `Click count: ${event.detail}`;
+// });
 
 
 
-(function () {
-    "use strict";
-  
-    /**
-     * Easy selector helper function
-     */
-    const select = (eli, all = false) => {
-      eli = eli.trim()
-      if (all) {
-        return [...document.querySelectorAll(eli)]
-      } else {
-        return document.querySelector(eli)
-      }
-    }
-  
-    /**
-     * Easy event listener function
-     */
-    const on = (type, eli, listener, all = false) => {
-      let selectEli = select(eli, all)
-  
-      if (selectEli) {
-        if (all) {
-          selectEli.forEach(e => e.addEventListener(type, listener))
-        } else {
-          selectEli.addEventListener(type, listener)
-        }
-      }
-    }
-  
-    /**
-     * Scrolls to an element with header offset
-     */
-    const scrollto = (eli) => {
-      window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-      })
-    }
-  
-    /**
-     * Mobile nav toggle
-     */
-    on('click', '.mobile-nav-toggle', function(e) {
-      select('#navbar').classList.toggle('navbar-mobile')
-      this.classList.toggle('bi-list')
-      this.classList.toggle('bi-x')
-    })
-  
-    /**
-     * Scrool with ofset on links with a class name .scrollto
-     */
-    on('click', '#navbar .nav-item', function(e) {
-      let section = select(this.hash)
-      if (section) {
-        e.preventDefault()
-  
-        let navbar = select('#navbar')
-        let header = select('#header')
-        let sections = select('section', true)
-        let navlinks = select('#navbar .nav-item', true)
-  
-        navlinks.forEach((item) => {
-          item.classList.remove('active')
-        })
-  
-        this.classList.add('active')
-  
-        if (navbar.classList.contains('navbar-mobile')) {
-          navbar.classList.remove('navbar-mobile')
-          let navbarToggle = select('.mobile-nav-toggle')
-          navbarToggle.classList.toggle('bi-list')
-          navbarToggle.classList.toggle('bi-x')
-        }
-  
-        if (this.hash == '#header') {
-          header.classList.remove('header-top')
-          sections.forEach((item) => {
-            item.classList.remove('section-show')
-          })
-          return;
-        }
-  
-        if (!header.classList.contains('header-top')) {
-          header.classList.add('header-top')
-          setTimeout(function() {
-            sections.forEach((item) => {
-              item.classList.remove('section-show')
-            })
-            section.classList.add('section-show')
-  
-          }, 350);
-        } else {
-          sections.forEach((item) => {
-            item.classList.remove('section-show')
-          })
-          section.classList.add('section-show')
-        }
-  
-        scrollto(this.hash)
-      }
-    }, true)
-  
-    /**
-     * Activate/show sections on load with hash links
-     */
-    window.addEventListener('load', () => {
-      if (window.location.hash) {
-        let initial_nav = select(window.location.hash)
-  
-        if (initial_nav) {
-          let header = select('#header')
-          let navlinks = select('#navbar .nav-item', true)
-  
-          header.classList.add('header-top')
-  
-          navlinks.forEach((item) => {
-            if (item.getAttribute('href') == window.location.hash) {
-              item.classList.add('active')
-            } else {
-              item.classList.remove('active')
-            }
-          })
-  
-          setTimeout(function() {
-            initial_nav.classList.add('section-show')
-          }, 350);
-  
-          scrollto(window.location.hash)
-        }
-      }
-    });
-    
-    new PureCounter();
 
-})()

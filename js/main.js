0a6db5114a716d67976effcc8a18c56454c03e8d
dilushaeka <dilushaeka99@gmail.com>
2023-06-10 09:58:15 +0530

    Add View Button For My Works

--- js/main.js
+++ js/main.js
@@ -12,6 +12,17 @@ window.addEventListener("scroll", function () {
 //   button.textContent = `Click count: ${event.detail}`;
 // });
 
+const hambuger = document.querySelector(".hamburger");
 
+const navList=document.querySelector(".nav-list")
 
+hambuger.addEventListener("clicked", () => {
+  hambuger.classList.toggle("active");
+  navList.classList.toggle("active");
+})
 
+document.querySelectorAll(".navList").forEach(n =>
+  n.addEventListener("clicked", () => {
+    hambuger.classList.remove("active");
+    navList.classList.remove("active");
+  }))
